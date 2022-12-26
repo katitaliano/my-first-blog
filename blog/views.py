41817d961ebf0d3b959f5aa503a641240aeb9766
kat.italiano@gmail.com <kat.italiano>
2022-12-26 16:58:04 +0800

    First URL pattern, view, template and html page added.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
+# function (def) called post_list that takes a request and will return the value it gets from calling another function - render - that will render (put together) our template blog/post_list.html.
